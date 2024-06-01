611d69f26a0936bc151b5e80102ce0874a7df328
jcartronics <jcar-95@hotmail.com>
2024-06-01 15:14:15 -0400

    Agrega rutas

--- proyectofinal-tienda/src/App.jsx
+++ proyectofinal-tienda/src/App.jsx
@@ -1,15 +1,42 @@
-
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 
 // Paginas
 import Home from './pages/Home'
+import AgregarDireccion from './pages/AgregarDireccion'
+import AgregarProducto from './pages/AgregarProducto'
+import Carrito from './pages/Carrito'
+import DetalleProducto from './pages/DetalleProducto'
+import EditarPerfil from './pages/EditarPerfil'
+import Favoritos from './pages/Favoritos'
+import Login from './pages/Login'
+import Productos from './pages/Productos'
+import Registro from './pages/Registro'
+import NotFound from './pages/NotFound'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/agregar-direccion', element: <AgregarDireccion /> },
+    { path: '/agregar-producto', element: <AgregarProducto /> },
+    { path: '/carrito', element: <Carrito /> },
+    { path: '/detalle-producto', element: <DetalleProducto /> },
+    { path: '/editar-perfil', element: <EditarPerfil /> },
+    { path: '/favoritos', element: <Favoritos /> },
+    { path: '/login', element: <Login /> },
+    { path: '/productos', element: <Productos /> },
+    { path: '/registro', element: <Registro /> },
+    { path: '*', element: <NotFound />}
+  ])
+  return routes
+}
+
+const App = () => {
 
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
---
+++ proyectofinal-tienda/src/pages/NotFound/index.jsx
@@ -0,0 +1,8 @@
+
+export const NotFound = () => {
+    return (
+      <div>Soy la página NotFound</div>
+    )
+  }
+  
+  export default NotFound
