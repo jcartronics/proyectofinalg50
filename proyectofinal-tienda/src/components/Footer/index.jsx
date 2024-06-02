ab424eb2b1508716caf56a86ab681429de553d78
jcartronics <jcar-95@hotmail.com>
2024-06-02 17:21:29 -0400

    Agrega Footer Base

--- proyectofinal-tienda/src/components/Footer/index.jsx
+++ proyectofinal-tienda/src/components/Footer/index.jsx
@@ -1,7 +1,21 @@
+import {NavLink} from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <div className="footer_container">Footer</div>
+    <div className="footer_container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-muted">© 2024 TronicsStore</p>
+
+        <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+          TroniscStore
+        </a>
+
+        <ul className="nav col-md-4 justify-content-end">
+
+          <NavLink to="/home" className="nav-link px-2 text-muted">Home</NavLink>
+        </ul>
+      </footer>
+    </div>
   )
 }
 
