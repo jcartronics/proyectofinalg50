4d28d1b61a1be47c412fb20331061da45938ab3f
jcartronics <jcar-95@hotmail.com>
2024-06-02 17:23:49 -0400

    Agrega Mayuscula a Logo

--- proyectofinal-tienda/src/components/Footer/index.jsx
+++ proyectofinal-tienda/src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
         <p className="col-md-4 mb-0 text-muted">© 2024 TronicsStore</p>
 
         <NavLink to="/home" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-          Tronicstore
+          TronicStore
         </NavLink>
 
         <ul className="nav col-md-4 justify-content-end">
